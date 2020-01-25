f4be4bd8f1e52ff75cdb0903d8a1d66804bbdf4c
teja <tejareddy.teju@gmail.com>
2020-01-26 00:57:08 +0530

    DOM class and id

--- js-bootcamp/notes-app/notes-app.js
+++ js-bootcamp/notes-app/notes-app.js
@@ -9,6 +9,23 @@ const notes = [{
     body: 'Get a new seat'
 }]
 
-document.querySelector('button').addEventListener('click', function (event) {
+document.querySelector('#create-note').addEventListener('click', function (event) {
     event.target.textContent = 'The button was clicked'
 })
+
+document.querySelector('#remove-all').addEventListener('click', function (e) {
+    document.querySelectorAll('.note').forEach(function (note) {
+        note.remove()
+    })
+})
+
+// -- single --
+// p
+// #replace
+// .item
+
+// -- multiple --
+// p#order
+// button.inventory
+// h1#title.application
+// h1.application#title
