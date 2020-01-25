af75efc4ddddfa3dcb65b344644743368647e8ac
teja <tejareddy.teju@gmail.com>
2020-01-26 02:33:28 +0530

    html render notes

--- js-bootcamp/todo-app/todo-app.js
+++ js-bootcamp/todo-app/todo-app.js
@@ -19,15 +19,27 @@ const incompleteTodos = todos.filter(function (todo) {
     return !todo.completed
 })
 
-const summary = document.createElement('h2')
-summary.textContent = `You have ${incompleteTodos.length} todos left`
-document.querySelector('body').appendChild(summary)
+const filterTodo = {
+    searchText: ''
+}
 
-todos.forEach(function (todo) {
-    const p = document.createElement('p')
-    p.textContent = todo.text
-    document.querySelector('body').appendChild(p)
-})
+const renderTodos = function (incompleteTodos, filterTodo) {
+    const renderTodos = incompleteTodos.filter(function (todo) {
+        return todo.text.toLowerCase().includes(filterTodo.searchText.toLowerCase())
+    })
+
+    document.querySelector('#todos').innerHTML = ''
+
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${incompleteTodos.length} todos left`
+    document.querySelector('#todos').appendChild(summary)
+
+    renderTodos.forEach(function (todo) {
+        const todoEl = document.createElement('p')
+        todoEl.textContent = todo.text
+        document.querySelector('#todos').appendChild(todoEl)
+    })
+}
 
 document.querySelector('#add-todo').addEventListener('click', function (event) {
     console.log('Add a new todo')
@@ -35,4 +47,9 @@ document.querySelector('#add-todo').addEventListener('click', function (event) {
 
 document.querySelector('#new-todo').addEventListener('input', function (e) {
     console.log(e.target.value)
+})
+
+document.querySelector('#search-todo').addEventListener('input', function (e) {
+    filterTodo.searchText = e.target.value
+    renderTodos(incompleteTodos, filterTodo)
 })
