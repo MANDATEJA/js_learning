32eaa370edf39bfe26526ecd4ef9499c56a4b541
teja <tejareddy.teju@gmail.com>
2020-02-19 13:58:51 +0530

    js XMLHttpRequest

--- js-bootcamp/hangman/app.js
+++ js-bootcamp/hangman/app.js
@@ -5,9 +5,40 @@ const game1 = new Hangman('cat', 2)
 puzzleElement.textContent = game1.puzzle
 gameStatusElement.textContent = game1.gameStatus
 
-window.addEventListener('keypress', function (e) {
+window.addEventListener('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode)
     game1.makeGuess(guess)
     puzzleElement.textContent = game1.puzzle
     gameStatusElement.textContent = game1.gameStatus
 })
+
+// Making an HTTP request
+const request = new XMLHttpRequest()
+
+request.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+        const data = JSON.parse(e.target.responseText)
+        console.log(data)
+    } else if (e.target.readyState === 4) {
+        console.log('An error has occured!')
+    }
+})
+
+request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
+request.send()
+
+const requestTwo = new XMLHttpRequest()
+
+requestTwo.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+        const data = JSON.parse(e.target.responseText)
+        const countryInfo = data.find((country) => country.alpha2Code === 'IN')
+
+        console.log(countryInfo.name)
+    } else if (e.target.readyState === 4) {
+        console.log('An error has occured!')
+    }
+})
+
+requestTwo.open('GET', 'http://restcountries.eu/rest/v2/all')
+requestTwo.send()
