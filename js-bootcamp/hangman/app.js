c543f72c27ee0ac35b79d117ef3749571e1aaabb
teja <tejareddy.teju@gmail.com>
2020-02-20 13:39:00 +0530

    js callback abstraction

--- js-bootcamp/hangman/app.js
+++ js-bootcamp/hangman/app.js
@@ -12,7 +12,7 @@ window.addEventListener('keypress', (e) => {
     gameStatusElement.textContent = game1.gameStatus
 })
 
-getPuzzle((error, puzzle) => {
+getPuzzle('2', (error, puzzle) => {
     if (error) {
         console.log(`Error: ${error}`)
     } else {
@@ -20,22 +20,10 @@ getPuzzle((error, puzzle) => {
     }
 });
 
-
-// Making an HTTP request
-
-
-// const requestTwo = new XMLHttpRequest()
-
-// requestTwo.addEventListener('readystatechange', (e) => {
-//     if (e.target.readyState === 4 && e.target.status === 200) {
-//         const data = JSON.parse(e.target.responseText)
-//         const countryInfo = data.find((country) => country.alpha2Code === 'IN')
-
-//         console.log(countryInfo.name)
-//     } else if (e.target.readyState === 4) {
-//         console.log('An error has occured!')
-//     }
-// })
-
-// requestTwo.open('GET', 'http://restcountries.eu/rest/v2/all')
-// requestTwo.send()
+getCountry('US', (error, country) => {
+    if (error) {
+        console.log(`Error: ${error}`)
+    } else {
+        console.log(country)
+    }
+})
