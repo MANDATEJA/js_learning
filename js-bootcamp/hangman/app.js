48dca8254b515a02d7dbfe9d21f9df9708cbb4ed
teja <tejareddy.teju@gmail.com>
2020-02-20 13:02:21 +0530

    js callback abstraction

--- js-bootcamp/hangman/app.js
+++ js-bootcamp/hangman/app.js
@@ -12,33 +12,30 @@ window.addEventListener('keypress', (e) => {
     gameStatusElement.textContent = game1.gameStatus
 })
 
+getPuzzle((error, puzzle) => {
+    if (error) {
+        console.log(`Error: ${error}`)
+    } else {
+        console.log(puzzle)
+    }
+});
+
+
 // Making an HTTP request
-const request = new XMLHttpRequest()
 
-request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText)
-        console.log(data)
-    } else if (e.target.readyState === 4) {
-        console.log('An error has occured!')
-    }
-})
 
-request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
-request.send()
+// const requestTwo = new XMLHttpRequest()
 
-const requestTwo = new XMLHttpRequest()
+// requestTwo.addEventListener('readystatechange', (e) => {
+//     if (e.target.readyState === 4 && e.target.status === 200) {
+//         const data = JSON.parse(e.target.responseText)
+//         const countryInfo = data.find((country) => country.alpha2Code === 'IN')
 
-requestTwo.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText)
-        const countryInfo = data.find((country) => country.alpha2Code === 'IN')
+//         console.log(countryInfo.name)
+//     } else if (e.target.readyState === 4) {
+//         console.log('An error has occured!')
+//     }
+// })
 
-        console.log(countryInfo.name)
-    } else if (e.target.readyState === 4) {
-        console.log('An error has occured!')
-    }
-})
-
-requestTwo.open('GET', 'http://restcountries.eu/rest/v2/all')
-requestTwo.send()
+// requestTwo.open('GET', 'http://restcountries.eu/rest/v2/all')
+// requestTwo.send()
