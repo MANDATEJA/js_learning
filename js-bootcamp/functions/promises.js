4dd30e880fac7f99939cdfbef82bdced63be37df
teja <tejareddy.teju@gmail.com>
2020-02-22 16:10:01 +0530

    js promises

---
+++ js-bootcamp/functions/promises.js
@@ -0,0 +1,29 @@
+// Callback
+const getDataCallback = (callback) => {
+    setTimeout(() => {
+        callback(undefined, 'This is the data!')
+    }, 2000)
+}
+
+getDataCallback((err, data) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(data)
+    }
+})
+
+// Promise
+const getDataPromise = (data) => new Promise((resolve, reject) => { // Using shorthand syntax
+    setTimeout(() => {
+        resolve(`This is my success data: ${data}`)
+        // reject('This is my promise error!')
+    }, 2000);
+})
+const myPromise = getDataPromise(123)
+
+myPromise.then((data) => {
+    console.log(data)
+}, (err) => {
+    console.log(err)
+})
--- js-bootcamp/hangman/requests.js
+++ js-bootcamp/hangman/requests.js
@@ -1,32 +1,32 @@
-const getPuzzle = (wordCount, callback) => {  
+const getPuzzle = (wordCount) => new Promise((resolve, reject) => {
     const request = new XMLHttpRequest()
 
     request.addEventListener('readystatechange', (e) => {
         if (e.target.readyState === 4 && e.target.status === 200) {
             const data = JSON.parse(e.target.responseText)
-            callback(undefined, data.puzzle)
+            resolve(data.puzzle)
         } else if (e.target.readyState === 4) {
-            callback('An error has occured!', undefined)
+            reject('An error has occured!')
         }
     })
     
     request.open('GET', `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
     request.send()
-}
+})
 
-const getCountry = (countryCode, callback) => {
+const getCountry = (countryCode) => new Promise((resolve, reject) => {
     const request = new XMLHttpRequest()
     request.addEventListener('readystatechange', (e) => {
         if (e.target.readyState === 4 && e.target.status === 200) {
             const data = JSON.parse(e.target.responseText)
             const countryInfo = data.find((country) => country.alpha2Code === countryCode)
 
-            callback(undefined, countryInfo.name)
+            resolve(countryInfo.name)
         } else if (e.target.readyState === 4) {
-            callback('An error has occured!', undefined)
+            reject('An error has occured!')
         }
     })
 
     request.open('GET', 'http://restcountries.eu/rest/v2/all')
     request.send()
-}
+})
