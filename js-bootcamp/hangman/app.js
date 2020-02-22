d37a54becf864bfa2dd2825e33b2650fa76aca39
teja <tejareddy.teju@gmail.com>
2020-02-23 01:44:37 +0530

    Wiring up hangman

--- js-bootcamp/hangman/app.js
+++ js-bootcamp/hangman/app.js
@@ -1,25 +1,36 @@
 const puzzleElement = document.querySelector('#puzzle')
 const gameStatusElement = document.querySelector('#game-status')
-
-const game1 = new Hangman('cat', 2)
-puzzleElement.textContent = game1.puzzle
-gameStatusElement.textContent = game1.gameStatus
+let game1
 
 window.addEventListener('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode)
     game1.makeGuess(guess)
+    render()
+})
+
+const render = () => {
     puzzleElement.textContent = game1.puzzle
     gameStatusElement.textContent = game1.gameStatus
-})
+}
 
-getPuzzle('2').then((puzzle) => {
-    console.log(puzzle)
-}).catch((err) => {
-    console.log(err)
-})
+const startGame = async () => {
+    const puzzle = await getPuzzle('2')
+    game1 = new Hangman(puzzle, 5)
+    render()
+}
 
-getCurrentCountry().then((country) => {
-    console.log(`Country: ${country}`)
-}).catch((err) => {
-    console.log(err)
-})
+document.querySelector('#reset').addEventListener('click', startGame) // Here we are referencing startGame function which gets called at line 22
+
+startGame()
+
+// getPuzzle('2').then((puzzle) => {
+//     console.log(puzzle)
+// }).catch((err) => {
+//     console.log(err)
+// })
+
+// getCurrentCountry().then((country) => {
+//     console.log(`Country: ${country}`)
+// }).catch((err) => {
+//     console.log(err)
+// })
