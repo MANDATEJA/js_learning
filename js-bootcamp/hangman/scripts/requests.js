886ae0dfe41717f2a917f19a4b8cd755e75cabfc
teja <tejareddy.teju@gmail.com>
2020-03-04 12:50:12 +0530

    refactoring request.js

--- js-bootcamp/hangman/scripts/requests.js
+++ js-bootcamp/hangman/scripts/requests.js
@@ -1,5 +1,5 @@
 const getPuzzle = async (wordCount) => {
-    const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`, {})
+    const response = await fetch(`//puzzle.mead.io/puzzle?wordCount=${wordCount}`, {})
     
     if (response.status === 200) {
         const data = await response.json()
@@ -10,7 +10,7 @@ const getPuzzle = async (wordCount) => {
 }
 
 const getCountry = async (countryCode) => {
-    const response = await fetch('http://restcountries.eu/rest/v2/all', {})
+    const response = await fetch('//restcountries.eu/rest/v2/all', {})
 
     if (response.status === 200) {
         const data = await response.json()
@@ -21,7 +21,7 @@ const getCountry = async (countryCode) => {
 }
 
 const getLocation = async () => {
-    const response = await fetch('http://ipinfo.io/json?token=********')
+    const response = await fetch('//ipinfo.io/json?token=***************')
 
     if (response.status === 200) {
         return response.json()
