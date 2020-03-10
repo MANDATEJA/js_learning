e4fbf0d485fd8bfe315f42f13c880979adf57fdd
teja <tejareddy.teju@gmail.com>
2020-03-10 16:14:28 +0530

    updating todo.js function

--- js-bootcamp/todo-app/src/todos.js
+++ js-bootcamp/todo-app/src/todos.js
@@ -1,4 +1,49 @@
-let todos = []
+import uuidv4 from 'uuid/v4'
+
+let todos = loadTodos()
+
+const loadTodos = () => {
+    const todosJSON = localStorage.getItem('todos')
+
+    try {
+        return todosJSON ? JSON.parse(todosJSON) : []
+    } catch (e) {
+        return []
+    }
+}
+
+const saveTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+}
+
+const getTodos = () => todos
+
+const createTodo = (text) => {
+    todos.push({
+        id: uuidv4(),
+        text,
+        completed: false
+    })
+    saveTodos()
+}
+
+const removeTodo = (id) => {
+    const todoIndex = todos.findIndex((todo) => todo.id === id)
+
+    if (todoIndex > -1) {
+        todos.splice(todoIndex, 1)
+    }
+}
+
+const toggleTodo = (id) => {
+    const todo = todos.find((todo) => todo.id === id)
+
+    if (todo) {
+        todo.completed = !todo.completed
+    }
+}
+
+export { loadTodos, saveTodos, getTodos, createTodo, removeTodo, toggleTodo }
 
 // Setup the empty todos array
 
