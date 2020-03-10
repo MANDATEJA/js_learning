afda36c225c572c826c7835fe1c9f145547b4fd4
teja <tejareddy.teju@gmail.com>
2020-03-10 15:54:33 +0530

    todo-app recommit

---
+++ js-bootcamp/todo-app/src/filters.js
@@ -0,0 +1,32 @@
+const filters = {
+    searchText: '',
+    hideCompleted: false
+}
+
+const getFilters = () => {
+    return filters
+}
+
+const setFilters = (updates) => {
+    if (typeof updates.searchText === 'string') {
+        filters.searchText = updates.searchText
+    }
+
+    if (typeof updates.hideCompleted === 'boolean') {
+        filters.hideCompleted = updates.hideCompleted
+    }
+}
+
+export { getFilters, setFilters }
+
+// Set up filters default 
+
+// getFilters
+// Arguments: none
+// Return value: filters object
+
+// setFilters
+// Arguments: updates object with optional searchText or hideCompleted
+// Return value: none
+
+// Make sure to set up the exports
